68b98b9e1a633a846cc148fcab2685a6c49712f7
shrey1108 <shrey.12020715@lpu.in>
2023-01-31 03:05:45 +0530

    until stack

---
+++ stack/stackstring.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int stack1(string s){
+    stack<string> st;
+    string word;
+    for(int i=0;i<s.size();i++){
+    //   word+=s[i];
+    //   if(s[i]==' '){
+    //   st.push(word);
+    //   word="";
+      
+    //   }
+    // if(i==s.size()-1){
+    //     st.push(word);
+    //     word="";
+    //   }
+    
+    word=s[i];
+    st.push(word);
+    word="";
+      }
+      while(!st.empty()){
+        for(int j=0;j<st.size();j++){
+            cout<<st.top();
+            st.pop();
+        }
+      }
+    }
+    int main(){
+        string s="hello your";
+        // cin>>s;
+        stack1(s);
+    }
